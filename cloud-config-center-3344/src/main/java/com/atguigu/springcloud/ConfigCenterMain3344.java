23eb44d1114edf5415e868616e1fef984396f6fa
qq <qq@qq.com>
2021-10-07 00:14:23 +0800

    xxx

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alycous
+ * @Date 2021-10-7 0:12:55
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
